49c6b704f5ce35849050796ef802caed5abd418c
Özgün <ergenozgun@gmail.com>
2017-03-06 23:17:18 +0200

    ekrana görüntü cizilmesi yapıldı

--- GetirHackathon/ViewController.swift
+++ GetirHackathon/ViewController.swift
@@ -7,13 +7,14 @@
 //
 
 import UIKit
+import FlatColor
 
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
-        self.view.backgroundColor = UIColor.whiteColor()
+        self.view.backgroundColor = UIColor.hexColor(0x212121)
         
         let param = ["email":"ergenozgun@gmail.com",
                      "name":"Orhan Özgün Ergen",
@@ -21,6 +22,7 @@ class ViewController: UIViewController {
         ]
         RequestConnenction.sharedInstance().connectionPOST(Service.getElements, parameter: param, complateBlock: { (json) in
             
+            print(json)
                 ApiParse.parseElements(json, complate: { (obj) in
                     
                         self.drawElement(obj)
@@ -35,16 +37,61 @@ class ViewController: UIViewController {
         
     }
 
+    override func viewWillAppear(animated: Bool) {
+
+        self.navigationController?.navigationBarHidden = true
+        
+
+    }
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
-    
     
     func drawElement(obj:Elements){
-    
         
+        for obj in obj.array_Element{
+        
+            if(obj.type == ElementType.circle){
+                
+                let circle = obj as! Circle
+                
+                let circlePath = UIBezierPath(arcCenter: CGPoint(x: circle.position.x,y: circle.position.y), radius: CGFloat(circle.r), startAngle: CGFloat(0), endAngle:CGFloat(M_PI * 2), clockwise: true)
+                
+                let shapeLayer = CAShapeLayer()
+                shapeLayer.path = circlePath.CGPath
+                shapeLayer.fillColor = UIColor.clearColor().CGColor
+                shapeLayer.strokeColor = getRandomColor().CGColor
+                shapeLayer.lineWidth = 3.0
+                view.layer.addSublayer(shapeLayer)
+            
+            }else{
+            
+            let rectangle = obj as! Rectangle
+                let vw_Rct = UIView()
+                vw_Rct.frame = CGRect(x: rectangle.position.x, y: rectangle.position.y, width: rectangle.width, height: rectangle.height)
+                vw_Rct.backgroundColor = UIColor.clearColor()
+                vw_Rct.layer.borderWidth = 1
+                vw_Rct.layer.borderColor = getRandomColor().CGColor
+                
+                
+                self.view.addSubview(vw_Rct)
+                
+                
+                
+            }
+        
+        }
+    }
+    
+    func getRandomColor() -> UIColor{
+        //Generate between 0 to 1
+        let red:CGFloat = CGFloat(drand48())
+        let green:CGFloat = CGFloat(drand48())
+        let blue:CGFloat = CGFloat(drand48())
+        
+        return UIColor(red:red, green: green, blue: blue, alpha: 1.0)
     }
 
 }
